1b637a555ff9871b11df3973c119b721ffd36a84
Ujjwal jain <ujjwaljainmain@gmail.com>
2024-07-15 21:53:29 +0530

    understand the meanign of FR and deleted repititive code files

--- 01_Day/featureRequest2.js
+++
@@ -1,9 +0,0 @@
-/*2.Reassignment Demo: Create a script that demonstrates the difference 
-in behavior between let and const when it comes to reassignment.*/
-let no = 4;
-console.log(`Before reassigning:`, no);
-no = 6;
-console.log(`After reassigning:`, no);
-const no2 = 4;
-console.log(`Before reassigning:`, no2);
-//no2=6 error is Type error:assgnment to constant variable
--- 02_Day/featureRequest3.js
+++
@@ -1,4 +0,0 @@
-function posOrNeg(num) {
-    return (num>0 ? `positive`:`Negative`)
-}
-console.log(posOrNeg(8));
