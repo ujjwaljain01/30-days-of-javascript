d747c9f3c05bfa8efe74b2492eea00d017c93d96
Ujjwal jain <ujjwaljainmain@gmail.com>
2024-07-19 21:36:54 +0530

    improve code readability of Day 05

--- 05_Day/functions.js
+++ 05_Day/functions.js
@@ -1,10 +1,12 @@
 //Activity 1: Function Declaration
+//- Task 1: Write a function to check if a number is even or odd and log the result to the console.
 console.log(`--------------TASK 1--------------`);
 function evenOdd(num) {
     return (num%2===0 ? `even`: `odd`)
 }
 console.log(evenOdd(14));
 
+//- Task 2: Write a function to calculate the square of a number and return the result.
 console.log(`--------------TASK 2--------------`);
 function square(num) {
     return num*num
@@ -12,6 +14,7 @@ function square(num) {
 console.log(square(5));
 
 // Activity 2: Function Expression
+//- Task 3: Write a function expression to find the maximum of two numbers and log the result to the console.
 console.log(`--------------TASK 3------------`);
 function max(num1,num2) {
     if(num1>num2){
@@ -26,6 +29,7 @@ function max(num1,num2) {
 }
 console.log(max(1,2));
 
+//- Task 4: Write a function expression to concatenate two strings and return the result.
 console.log(`--------------TASK 4--------------`);
 function concate(str1,str2) {
     return str1+str2
@@ -33,12 +37,14 @@ function concate(str1,str2) {
 console.log(concate("Hello ","World"));
 
 //Activity 3: Arrow Functions
+//- Task 5: Write an arrow function to calculate the sum of two numbers and return the result.
 console.log(`--------------TASK 5--------------`);
 let twoSum=(num1,num2)=>{
     return num1+num2
 }
 console.log(twoSum(1,2));
 
+//- Task 6: Write an arrow function to check if a string contains a specific character and return a boolean value.
 console.log(`--------------TASK 6--------------`);
 let check=(str,char)=>{
     if(str.includes(char)){
@@ -49,12 +55,14 @@ let check=(str,char)=>{
 console.log(check("func1","h"));
 
 //Activity 4: Function Parameters and Default Values
+//- Task 7: Write a function that takes two parameters and returns their product. Provide a default value for the second parameter.
 console.log(`--------------TASK 7--------------`);
 let product=(num1,num2=2)=>{
     return num1*num2
 }
 console.log(product(2));
 
+//- Task 8: Write a function that takes a person's name and age and returns a greeting message. Provide a default value for the age.
 console.log(`--------------TASK 8--------------`);
 let greeting=(name,age=19)=>{
     return `Hello ${name}, Welcome to our office ${age}`
@@ -62,6 +70,7 @@ let greeting=(name,age=19)=>{
 console.log(greeting("Ujjwal"));
 
 //Activity 5: Higher-Order Functions
+//- Task 9: Write a higher-order function that takes a function and a number, and calls the function that many times.
 console.log(`--------------TASK 9--------------`);
 function higher(func,num) {
     for (let i = 0; i < num; i++) {
@@ -73,6 +82,7 @@ function call() {
 }
 higher(call,3)
 
+// Task 10: Write a higher-order function that takes two functions and a value, applies the first function to the value, and then applies the second function to the result.
 console.log(`--------------TASK 10--------------`);
 function higherOrder(func1,func2,str){
     const resultfromfunc1= func1(str)
